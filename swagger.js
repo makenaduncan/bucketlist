4ab1b5a7dd922e3092282551c765976db791a57d
makena <mduncan18@byui.edu>
2022-05-21 19:37:34 -0600

    attempt who knows

--- swagger.js
+++ swagger.js
@@ -5,11 +5,11 @@ const doc = {
     title: "My API",
     description: "Description",
   },
-  host: "bucketlist341.herokuapp.com",
+  host: "bucketlist341.herokuapp.com/bucketlist",
   schemes: ["https"],
 };
 
-const outputFile = "swagger-output.json/bucketlist";
+const outputFile = "swagger-output.json";
 const endpointsFiles = ["./server.js"];
 
 /* NOTE: if you use the express Router, you must pass in the 
