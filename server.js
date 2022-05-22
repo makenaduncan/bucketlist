9fde597d7dedc9d982f9d3da7cf184671d129120
makena <mduncan18@byui.edu>
2022-05-21 19:54:51 -0600

    new attempt

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const mongodb = require("./db/connect");
+const cors = require("cors");
 const port = process.env.PORT || 8080;
 
 const bucketlistRoutes = require("./routes/bucketlistRoutes");
 const userRoutes = require("./routes/userRoutes");
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
