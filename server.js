5294b5b4ad146babcc97ced6185a9955f5181d4a
makena <mduncan18@byui.edu>
2022-05-21 19:27:36 -0600

    Hope this works!

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
-  );
+  // res.setHeader(
+  //   "Access-Control-Allow-Headers",
+  //   "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+  // );
   next();
 });
 app.use("/bucketlist", bucketlistRoutes);
